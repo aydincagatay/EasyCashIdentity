7767c378c04182892da900efb22e79e9105e0f8d
aydincagatay <cagatayaydinn@gmail.com>
2023-05-10 23:05:29 +0300

    AspUser and AspRole tables configured.

--- EasyCashIdentity.DataAccessLayer/Concrete/Context.cs
+++ EasyCashIdentity.DataAccessLayer/Concrete/Context.cs
@@ -9,13 +9,13 @@ using System.Threading.Tasks;
 
 namespace EasyCashIdentity.DataAccessLayer.Concrete
 {
-    public class Context : IdentityDbContext
+    public class Context : IdentityDbContext<AppUser, AppRole, int>
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
             optionsBuilder.UseSqlServer("server=CAGI\\SQLEXPRESS;initial catalog=EasyCashDb;integrated security=true;");
-            
+
         }
         public DbSet<CustomerAccount> CustomerAccounts { get; set; }
         public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; }
