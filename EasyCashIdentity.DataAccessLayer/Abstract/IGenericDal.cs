20392c6e4f72626a205e712ba616206bc0ffe638
aydincagatay <cagatayaydinn@gmail.com>
2023-05-23 00:01:31 +0300

    Repository pattern

---
+++ EasyCashIdentity.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentity.DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T : class
+    {
+        List<T> GetAllAsync();
+        T GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
---
+++ EasyCashIdentity.DataAccessLayer/Repository/GenericRepository.cs
@@ -0,0 +1,44 @@
+﻿using EasyCashIdentity.DataAccessLayer.Abstract;
+using EasyCashIdentity.DataAccessLayer.Concrete;
+
+namespace EasyCashIdentity.DataAccessLayer.Repository
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        public Task AddAsync(T entity)
+        {
+            using var context = new Context();
+            context.Set<T>().Add(entity);
+            context.SaveChanges();
+            return Task.CompletedTask; 
+        }
+
+        public Task DeleteAsync(T entity)
+        {
+            using var context = new Context();
+            context.Set<T>().Remove(entity);
+            context.SaveChanges();
+            return Task.CompletedTask;
+        }
+
+        public List<T> GetAllAsync()
+        {
+            using var context = new Context();
+            return context.Set<T>().ToList(); 
+        }
+
+        public T GetByIdAsync(int id)
+        {
+            using var context = new Context();
+            return context.Set<T>().Find(id);
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            var context = new Context();
+            context.Set<T>().Update(entity);
+            context.SaveChanges();
+            return Task.CompletedTask;
+        }
+    }
+}
